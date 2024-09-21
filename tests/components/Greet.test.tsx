786d37c1302531cec48001a4f517cc4bc706a905
Damilola Osunsanwo <82976159+ddddami@users.noreply.github.com>
2024-09-21 20:40:04 +0100

    Testing rendering

---
+++ tests/components/Greet.test.tsx
@@ -0,0 +1,23 @@
+import { it, expect, describe } from "vitest";
+import { render, screen } from "@testing-library/react";
+import Greet from "../../src/components/Greet";
+import "@testing-library/jest-dom/vitest";
+
+describe("Greet", () => {
+  it("should render hello with the name when name is provided", () => {
+    render(<Greet name="Dami" />);
+
+    const heading = screen.getByRole("heading");
+    expect(heading).toBeInTheDocument();
+    expect(heading).toHaveTextContent(/dami/i);
+    // screen.debug();
+  });
+
+  it("should render login button when name is not provided", () => {
+    render(<Greet />);
+
+    const button = screen.getByRole("button");
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveTextContent(/login/i);
+  });
+});
