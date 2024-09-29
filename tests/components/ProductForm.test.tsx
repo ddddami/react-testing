66fc4f59a30d592f298d96bb678523a5d8daf166
Damilola Osunsanwo <82976159+ddddami@users.noreply.github.com>
2024-09-29 20:22:23 +0100

    Exercise- testing validation rules

--- tests/components/ProductForm.test.tsx
+++ tests/components/ProductForm.test.tsx
@@ -52,29 +52,63 @@ describe("ProductForm", () => {
   });
 
   it.each([
-    { scenario: "name is missing", errorMessage: /required/i },
+    { scenario: "missing", errorMessage: /required/i },
     {
-      scenario: "name is longer than 255 characters",
+      scenario: "longer than 255 characters",
       errorMessage: /255/i,
       name: "a".repeat(256),
     },
-  ])("should display an error if $scenario", async ({ errorMessage, name }) => {
-    const { waitForFormToLoad } = renderComponent();
+  ])(
+    "should display an error if name is $scenario",
+    async ({ errorMessage, name }) => {
+      const { waitForFormToLoad } = renderComponent();
 
-    const form = await waitForFormToLoad();
-    const user = userEvent.setup();
-    if (name !== undefined) {
-      await user.type(form.nameInput, name);
+      const form = await waitForFormToLoad();
+      const user = userEvent.setup();
+      if (name !== undefined) {
+        await user.type(form.nameInput, name);
+      }
+      await user.type(form.priceInput, "10"),
+        await user.click(form.categoryInput);
+      const options = screen.queryAllByRole("option");
+      await user.click(options[0]);
+      await user.click(form.submitButton);
+
+      const error = screen.queryByRole("alert");
+      expect(error).toHaveTextContent(errorMessage);
     }
-    await user.type(form.priceInput, "10"),
-      await user.click(form.categoryInput);
-    const options = screen.queryAllByRole("option");
-    await user.click(options[0]);
-    await user.click(form.submitButton);
+  );
 
-    const error = screen.queryByRole("alert");
-    expect(error).toHaveTextContent(errorMessage);
-  });
+  it.each([
+    { scenario: "missing", errorMessage: /required/i },
+    { scenario: "< than 1", price: "0", errorMessage: /greater/i },
+    { scenario: "negative", price: "-1", errorMessage: /greater/i },
+    { scenario: "NaN", price: "a", errorMessage: /required/i },
+
+    {
+      scenario: "> than 1000",
+      price: "1001",
+      errorMessage: /less/i,
+    },
+  ])(
+    "should display an error if price is $scenario",
+    async ({ price, errorMessage }) => {
+      const { waitForFormToLoad } = renderComponent();
+
+      const form = await waitForFormToLoad();
+      const user = userEvent.setup();
+      await user.type(form.nameInput, "aaaa");
+      if (price !== undefined) await user.type(form.priceInput, price);
+
+      await user.click(form.categoryInput);
+      const options = screen.queryAllByRole("option");
+      await user.click(options[0]);
+      await user.click(form.submitButton);
+
+      const error = screen.queryByRole("alert");
+      expect(error).toHaveTextContent(errorMessage);
+    }
+  );
 });
 
 const renderComponent = (product?: Product) => {
