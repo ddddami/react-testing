3a738c8e7bf4c980eaed9ddfd9c47988dfe1f67b
Damilola Osunsanwo <82976159+ddddami@users.noreply.github.com>
2024-10-02 17:54:51 +0100

    Extracta reusable  navigateTo

---
+++ tests/utils.tsx
@@ -0,0 +1,25 @@
+import { render } from "@testing-library/react";
+import { delay, http, HttpResponse } from "msw";
+import { createMemoryRouter, RouterProvider } from "react-router-dom";
+import routes from "../src/routes";
+import { server } from "./mocks/server";
+
+export const simulateDelay = (endpoint: string) => {
+  server.use(
+    http.get(endpoint, async () => {
+      await delay();
+      return HttpResponse.json([]);
+    })
+  );
+};
+
+export const simulateError = (endpoint: string) => {
+  server.use(http.get(endpoint, () => HttpResponse.error()));
+};
+
+export const navigateTo = (path: string) => {
+  const router = createMemoryRouter(routes, {
+    initialEntries: [path],
+  });
+  render(<RouterProvider router={router} />);
+};
