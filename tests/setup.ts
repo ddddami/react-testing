6a6b71d894b16850495c152caed66945b809c97b
Damilola Osunsanwo <82976159+ddddami@users.noreply.github.com>
2024-10-02 16:59:03 +0100

    Mock the authentication status

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,10 +1,13 @@
 // setup file is run before each test file. good for configuring test environment
 
+import { useAuth0, User } from "@auth0/auth0-react";
 import "@testing-library/jest-dom/vitest";
 import ResizeObserver from "resize-observer-polyfill";
 import { server } from "./mocks/server";
 
 global.ResizeObserver = ResizeObserver;
+
+vi.mock("@auth0/auth0-react");
 
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
@@ -27,3 +30,21 @@ Object.defineProperty(window, "matchMedia", {
 window.HTMLElement.prototype.scrollIntoView = vi.fn();
 window.HTMLElement.prototype.hasPointerCapture = vi.fn();
 window.HTMLElement.prototype.releasePointerCapture = vi.fn();
+
+type AuthState = {
+  isAuthenticated: boolean;
+  isLoading: boolean;
+  user: User | undefined;
+};
+export const mockAuthState = (authState: AuthState) => {
+  return vi.mocked(useAuth0).mockReturnValue({
+    ...authState,
+    getAccessTokenSilently: vi.fn().mockResolvedValue({}),
+    getAccessTokenWithPopup: vi.fn(),
+    getIdTokenClaims: vi.fn(),
+    loginWithRedirect: vi.fn(),
+    loginWithPopup: vi.fn(),
+    logout: vi.fn(),
+    handleRedirectCallback: vi.fn(),
+  });
+};
